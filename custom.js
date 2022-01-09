dea07d3b2a0022005aedf183b3d0c6dc95b77032
Evan Partidas <evanpartidas@gmail.com>
2022-01-09 17:51:08 -0500

    Create a much nicer looking webpage

---
+++ custom.js
@@ -0,0 +1,9 @@
+var waitForEl = function(selector, callback) {
+  if (jQuery(selector).length) {
+    callback();
+  } else {
+    setTimeout(function() {
+      waitForEl(selector, callback);
+    }, 100);
+  }
+};
